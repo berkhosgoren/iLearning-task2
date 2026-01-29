7c2bf73f55dbf7e939b787cb9b81d6bcdfccd7af
berkhosgoren <berk.2000hosgoren@hotmail.com>
2026-01-29 16:14:08 +0300

    list input files and verify the count

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Linq;
+
+var folder = @"C:\Users\Berk\Desktop\task2";
+
+if (!Directory.Exists(folder))
+{
+    Console.WriteLine("Folder not found: " + folder);
+    return;
+}
+
+var files = Directory.GetFiles(folder).OrderBy(f => f, StringComparer.OrdinalIgnoreCase).ToArray();
+
+Console.WriteLine($"Files found: {files.Length}");
+
+if (files.Length > 0)
+{
+    Console.WriteLine("First few files:");
+    foreach (var f in files.Take(5)) Console.WriteLine(" - " + Path.GetFileName(f));  
+}
+
